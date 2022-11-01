606281c9476888ec3c10e8939a3ce21c2ce3bfe1
Cristina Dege <cris.dege0813@gmail.com>
2022-11-01 16:26:30 -0400

    home screens

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeViews from '../components/HomeViews';
+
+const Home = () => {
+  return (
+    <div className="flex-row">
+      <div className="col-12 mb-3">
+        <HomeViews />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
