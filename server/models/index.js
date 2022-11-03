4bc4bfa0b9b647b80eba6cceca5c220cfebb7c78
Max Taylor <maxbransontaylor@gmail.com>
2022-11-02 20:50:26 -0400

    EventTask model and all seeds done

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,7 @@
-const User = require('./User');
-const Department = require('./Department');
-const Company = require('./Company')
-const Event = require('./Event');
-const Location = require('./Location');
-module.exports = { User, Department, Company, Event, Location};
+const User = require("./User");
+const Department = require("./Department");
+const Company = require("./Company");
+const Event = require("./Event");
+const Location = require("./Location");
+const EventTask = require("./EventTask");
+module.exports = { User, Department, Company, Event, Location, EventTask };
