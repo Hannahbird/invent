d255d396b6644187c952bb94b76b80d6e62060e0
Max Kottong <max.kottong@gmail.com>
2022-11-03 20:51:19 -0400

    DepartmentEventList completed

--- client/src/components/DepartmentEventList/index.js
+++ client/src/components/DepartmentEventList/index.js
@@ -4,7 +4,7 @@ import { QUERY_DEPARTMENT_EVENTS } from '../../utils/queries';
 const DepartmentEventList = () => {
     const { loading, data } = useQuery(QUERY_DEPARTMENT_EVENTS);
 
-    const events = data?.events || {};
+    const events = data?.deptEvents || {};
 
     if (loading) {
         return <div>Loading...</div>;
@@ -17,4 +17,28 @@ const DepartmentEventList = () => {
             </>
         );
     };
+
+    return (
+        <div>
+            <Create />
+            <h3>Your Current Events</h3>
+            {events &&
+                events.map((event) => (
+                    <div key={event._id} className="card mb-3 col-6">
+                        <div className="card-header">
+                            <p>{event.eventName}</p>
+                        </div>
+                        <div className="card-body row">
+                            {event.eventDate}
+                            <br />
+                            {event.location.locationName}
+                            <br />
+                            {event.contactName}
+                            {event.contactInfo}
+                            {event.eventState}
+                        </div>
+                    </div>
+                ))}
+        </div>
+    );
 };
